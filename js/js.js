ebfe257bb5912d059d565ef665b7005f9e095fd9
Renee Cuda <renee.cuda05@gmail.com>
2015-07-24 15:17:57 +1000

    update 2
    update 2

--- js/js.js
+++ js/js.js
@@ -5,6 +5,6 @@ var accountBalance = 130;
 function PopulateModal(item, price)
 {
     $("#price").html("$" + price + "");
-    $("#" + item + "").css("background-color", "#D0D0D0 ");
+    $("#" + item + "").css("background-color", "#D0D0D0");
     $('#myModal').modal('show');
 }
