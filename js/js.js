70b11a620e8a6e4f770f7ac5f1051b439109ac75
Renee Cuda <renee.cuda05@gmail.com>
2015-07-24 14:42:54 +1000

    update

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
+var totalInvestment = 800;
+var accountBalance = 130;
+
+
 function PopulateModal(item, price)
 {
     $("#price").html("$" + price + "");
+    $("#" + item + "").css("background-color", "#D0D0D0 ");
     $('#myModal').modal('show');
 }
