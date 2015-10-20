be98157533a07a22dcbd62fdaa3bbf0c8e5e1730
Renee Cuda <renee.cuda05@gmail.com>
2015-10-20 15:57:12 +1100

    update for suez
    test

--- js/js.js
+++ js/js.js
@@ -8,3 +8,10 @@ function PopulateModal(item, price)
     $("#" + item + "").css("background-color", "#D0D0D0");
     $('#myModal').modal('show');
 }
+
+function SubmitForm()
+{
+    $("#modalTitle2").html("Thank you");
+    $("#modalDescription2").html("Someone will be in contact soon");
+    $("#form").hide();
+}
